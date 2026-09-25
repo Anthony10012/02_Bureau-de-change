a49672d301a1fcf5498e93845c497ce7aed9c695
Anthony10012 <Anthony.Simond@eduvaud.ch>
2026-09-25 14:05:36 +0200

    refactor: Opération de la somme et du solde du compte + Affichage du ticket

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ Remarque(s) :
 #include <iostream>
 #include <cstdlib>
 #include <windows.h>
+#include <cmath>
 
 using namespace std;
 
@@ -25,9 +26,9 @@ int main() {
       cout << "Quel est votre nom de famille ? " << endl;
       cin >> nom_utilisateur;
 
-      const int solde_compte = 1000; // En CHF
+      const double solde_compte = 1000.0; // En CHF
       const double taux_change = 1.024; // En Euro
-      const int frais_operation = 5; // En CHF
+      const double frais_operation = 5.0; // En CHF
       double somme_euro;
       cout << "Solde de votre compte CHF : " << solde_compte << endl;
       cout << "Taux de change : 1 CHF = " << taux_change << " Euro " << endl;
@@ -36,12 +37,26 @@ int main() {
       cout << "Entrez la somme souhaitée en Euro :" << endl;
       cin >> somme_euro;
 
+      double montant_franc = round((somme_euro / taux_change)*100.0) / 100.0; // Arrondi 2 chiffre après la virgule
+      double somme_chf = montant_franc + frais_operation;
+      double montant_restant = solde_compte - somme_chf;
+      cout << "Somme CHF : " << montant_franc << ", Solde compte : " << montant_restant << endl;
 
-
-
-
-
-
+      // Ticket
+      cout << "+----------------------------+" << endl;
+      cout <<"|" << endl;
+      cout <<"|" << nom_utilisateur << endl;
+      cout <<"|" << numero_compte_bancaire << endl;
+      cout <<"|" << endl;
+      cout <<"|" <<  "Somme Euro             : "<< somme_euro << endl;
+      cout <<"|" <<  "1 CHF en Euro          : " << taux_change << endl;
+      cout <<"|" << endl;
+      cout <<"|" <<  "Somme CHF              : " << montant_franc << endl;
+      cout <<"|" <<  "Frais                  : " << frais_operation << endl;
+      cout <<"|" << endl;
+      cout <<"|" <<  "Solde Compte           : " << montant_restant << endl;
+      cout <<"|" << endl;
+      cout << "+----------------------------+" << endl;
 
 
 }
